c46521ad5f2bd05f6424f0504f040db0d95aabc7
melissaprax <melissaprax@gmail.com>
2018-05-05 13:39:34 -0500

    sat at 1:40

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ let path = require('path');
 
 
 let app = express();
-let PORT = 3000;
+// let PORT = 3000;
+let PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
